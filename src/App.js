0f499f93f9c5cb425892a34b11d6c6aa5f4c1cb1
Steffen Hirte <shirte@users.noreply.github.com>
2024-10-20 20:59:42 +0200

    Add router

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { useEffect } from "react"
+import { useSelector } from "react-redux"
+import {
+    Outlet,
+    RouterProvider,
+    createBrowserRouter,
+    useLocation,
+} from "react-router-dom"
+import Debug from "./features/debug/Debug"
+import AboutPage from "./pages/AboutPage"
+import CitationPage from "./pages/CitationPage"
+import CreateJobPage from "./pages/CreateJobPage"
+import DeveloperPage from "./pages/DeveloperPage"
+import LandingPage from "./pages/LandingPage"
+import LoadingPage from "./pages/LoadingPage"
+import ResultsPage from "./pages/ResultsPage"
+
+function Root() {
+    const { pathname, hash } = useLocation()
+
+    // whenever the route changes, scroll to the top of the page
+    useEffect(() => {
+        // window.scrollTo(0, 0) does not work here, because the browser
+        // scrolls to the top of the page before the new content is rendered
+        // -> use setTimeout to scroll after the new content is rendered
+        if (hash === "") {
+            setTimeout(() => window.scrollTo(0, 0), 0)
+        }
+    }, [pathname])
+
+    // this is a hack to force a re-render of all components
+    // whenever the key changes, all components are re-rendered
+    const key = useSelector((state) => state.debug.key)
+
+    return (
+        <>
+            <Debug />
+            <Outlet key={key} />
+        </>
+    )
+}
+
+export default function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Root />,
+            children: [
+                {
+                    path: "/",
+                    id: "landing",
+                    element: <LandingPage />,
+                },
+                {
+                    path: "/:moduleName",
+                    id: "createJob",
+                    element: <CreateJobPage />,
+                },
+                {
+                    path: "/:moduleName/about",
+                    id: "about",
+                    element: <AboutPage />,
+                },
+                {
+                    path: "/:moduleName/api",
+                    id: "api",
+                    element: <DeveloperPage />,
+                },
+                {
+                    path: "/:moduleName/cite",
+                    id: "cite",
+                    element: <CitationPage />,
+                },
+                {
+                    // put this route at the bottom to avoid conflicts with other routes
+                    path: "/:moduleName/:jobId",
+                    id: "results",
+                    element: <ResultsPage />,
+                },
+                {
+                    // this route only exists for debugging purposes
+                    // (no link should point to this page)
+                    path: "/loading",
+                    id: "loading",
+                    element: <LoadingPage />,
+                },
+            ],
+        },
+    ])
+
+    return <RouterProvider router={router} />
+}
