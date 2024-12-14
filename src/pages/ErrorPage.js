79790c38d9dec45e0f6b614514c16bb2c3d33c60
Steffen Hirte <shirte@users.noreply.github.com>
2024-12-14 10:08:00 +0100

    Use localhost in proxy settings for now

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types"
 import React from "react"
 
-export default function ErrorPage({ errorMessage }) {
-    return <div>{errorMessage}</div>
+export default function ErrorPage({ message, error }) {
+    return (
+        <div>
+            {message} {JSON.stringify(error)}
+        </div>
+    )
 }
 
 ErrorPage.propTypes = {
-    errorMessage: PropTypes.string.isRequired,
+    message: PropTypes.string.isRequired,
+    error: PropTypes.object,
 }
