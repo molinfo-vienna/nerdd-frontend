45f289a85b31d0d59a15926e161b38657ec1b46b
Steffen Hirte <shirte@users.noreply.github.com>
2025-01-04 18:02:22 +1300

    Add propTypes for jobs

--- src/types/job.js
+++ src/types/job.js
@@ -1,5 +1,25 @@
 import PropTypes from "prop-types"
 
-const jobType = PropTypes.shape({})
+const jobType = PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    jobType: PropTypes.string.isRequired,
+    sourceId: PropTypes.string.isRequired,
+    params: PropTypes.object.isRequired,
+    createdAt: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+    numEntriesTotal: PropTypes.number,
+    numEntriesProcessed: PropTypes.number.isRequired,
+    numPagesTotal: PropTypes.number,
+    numPagesProcessed: PropTypes.number.isRequired,
+    pageSize: PropTypes.number,
+    outputFiles: PropTypes.arrayOf(
+        PropTypes.shape({
+            format: PropTypes.string.isRequired,
+            url: PropTypes.string.isRequired,
+        }),
+    ),
+    jobUrl: PropTypes.string.isRequired,
+    resultsUrl: PropTypes.string.isRequired,
+})
 
 export default jobType
