957ae09d30db03578a6164bd34d084624c365513
Steffen Hirte <shirte@users.noreply.github.com>
2025-01-04 18:02:06 +1300

    Let externl links open new tab

--- src/features/footer/FooterLink.js
+++ src/features/footer/FooterLink.js
@@ -2,10 +2,14 @@ import React from "react"
 import { Link } from "react-router-dom"
 import Icon from "../icon/Icon"
 
-export default function FooterLink({ icon, title, url }) {
+export default function FooterLink({ icon, title, url, ...props }) {
     return (
         <li className="mb-2">
-            <Link className="muted-link d-flex align-items-center" to={url}>
+            <Link
+                className="muted-link d-flex align-items-center"
+                to={url}
+                {...props}
+            >
                 <Icon collection="fa" size={18} name={icon} className="me-2" />
                 <span>{title}</span>
             </Link>
