9fc63b3d669b9d34ee5f276f559b328bd3da31bf
Steffen Hirte <shirte@users.noreply.github.com>
2025-01-20 11:31:28 +1300

    Add problem column in mock server

--- src/features/mockServer/fake/util.js
+++ src/features/mockServer/fake/util.js
@@ -1,5 +1,18 @@
 import { faker } from "@faker-js/faker"
 
+const problemTypes = [
+    "unknown_error",
+    "incomplete_prediction",
+    "kekulization_error",
+]
+
+function generateProblem() {
+    return [
+        faker.helpers.arrayElement(problemTypes),
+        faker.lorem.words({ min: 1, max: 4 }),
+    ]
+}
+
 export function generateValue(dataType) {
     if (dataType === "string") {
         return faker.lorem.words({ min: 1, max: 4 })
@@ -11,6 +24,9 @@ export function generateValue(dataType) {
         return faker.number.float({ min: -100, max: 100 })
     } else if (dataType === "mol") {
         return `/resources/fake/molecules/mol_${faker.number.int({ min: 0, max: 11 })}.svg`
+    } else if (dataType === "problem_list") {
+        const length = faker.number.int({ min: 0, max: 5 })
+        return Array.from({ length }, () => generateProblem())
     } else {
         console.log(dataType)
     }
