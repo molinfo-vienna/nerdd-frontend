84e9e46ae466f93292f8aab9a3ccc2223634e648
Steffen Hirte <shirte@users.noreply.github.com>
2025-03-15 22:20:48 +1300

    Memoize color palettes

--- src/features/colorPalettes/useColorPalettes.js
+++ src/features/colorPalettes/useColorPalettes.js
@@ -1,29 +1,35 @@
+import { useMemo } from "react"
 import d3Palettes from "./d3Palettes"
 import useDefaultPalettes from "./useDefaultPalettes"
 
 export default function useColorPalettes() {
     const defaultPalettes = useDefaultPalettes()
 
-    return {
-        null: defaultPalettes.null,
-        interpolators: {
-            ...d3Palettes.interpolators,
-        },
-        colors: {
-            ...(d3Palettes.colors ?? {}),
-            ...defaultPalettes.colors,
-        },
-        categorical: {
-            ...d3Palettes.categorical,
-            ...defaultPalettes.categorical,
-        },
-        sequential: {
-            ...d3Palettes.sequential,
-            ...defaultPalettes.sequential,
-        },
-        diverging: {
-            ...d3Palettes.diverging,
-            ...defaultPalettes.diverging,
-        },
-    }
+    const result = useMemo(
+        () => ({
+            null: defaultPalettes.null,
+            interpolators: {
+                ...d3Palettes.interpolators,
+            },
+            colors: {
+                ...(d3Palettes.colors ?? {}),
+                ...defaultPalettes.colors,
+            },
+            categorical: {
+                ...d3Palettes.categorical,
+                ...defaultPalettes.categorical,
+            },
+            sequential: {
+                ...d3Palettes.sequential,
+                ...defaultPalettes.sequential,
+            },
+            diverging: {
+                ...d3Palettes.diverging,
+                ...defaultPalettes.diverging,
+            },
+        }),
+        [defaultPalettes],
+    )
+
+    return result
 }
