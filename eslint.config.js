72fcd7d4b7a2d53920e769860df13d7582ab30d4
Steffen Hirte <shirte@users.noreply.github.com>
2025-03-21 23:42:33 +1300

    Migrate eslint to new eslint.config.js format

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      '@stylistic': stylistic,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      "prettier": eslintPluginPrettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      "prettier/prettier": [
+            "error",
+            {
+                "semi": false,
+                "tabWidth": 4,
+                "eslintIntegration": true
+            }
+        ],
+        
+        "@stylistic/semi": [
+            "error",
+            "never"
+        ],
+        "no-param-reassign": [
+            "error",
+            {
+                "props": false
+            }
+        ]
+    },
+  },
+)
