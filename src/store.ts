0bc7cddcb78ba029072c30fa23deb5d125559fa8
Steffen Hirte <shirte@users.noreply.github.com>
2025-03-23 00:32:35 +1300

    Convert redux store to typescript

--- src/store.ts
+++ src/store.ts
@@ -17,3 +17,6 @@ export const store = configureStore({
 })
 
 setupListeners(store.dispatch)
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
