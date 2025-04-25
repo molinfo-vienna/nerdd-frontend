c016af2e53807aedfcc9e8d22286a6dcff7cde2b
Steffen Hirte <shirte@users.noreply.github.com>
2025-04-25 15:22:19 +1200

    feat: Make app mobile again (MAMA)

--- src/features/resultsHeader/ResultsHeader.tsx
+++ src/features/resultsHeader/ResultsHeader.tsx
@@ -15,21 +15,27 @@ export default function ResultsHeader({
     const statusText = `${jobStatus.numEntriesProcessed ?? 0} / ${jobStatus.numEntriesTotal ?? "?"} molecules processed`
 
     return (
-        <section className="py-4 d-flex justify-content-center">
-            <div className="d-block d-lg-flex flex-wrap">
-                <div className="hstack pb-3">
+        <section className="d-flex justify-content-center py-4">
+            {/*
+             * Main idea:
+             * - d-lg-flex is used to align the content in a row on large screens
+             * - for small screens we use plain block (d-block) elements so the browser will
+             *   wrap content, but also align everything to the left
+             * - the encompassing section is centered, so the content block itself is centered
+             */}
+            <div className="d-block d-lg-flex">
+                <div
+                    // mx-1 for a small margin on small screens
+                    // mx-sm-3 to put the progress bar roughly above the first icon on large screens
+                    className="hstack pb-3 mx-1 mx-sm-3"
+                >
                     {/* Progress bar */}
                     <ProgressBar
                         numEntriesProcessed={jobStatus.numEntriesProcessed}
                         numEntriesTotal={jobStatus.numEntriesTotal}
                     />
-                    {/* Module name & status*/}
-                    <div
-                        className="d-flex flex-column justify-content-end py-2 me-5"
-                        style={{
-                            height: "90px",
-                        }}
-                    >
+                    {/* Module name & status */}
+                    <div className="d-flex flex-column py-3 me-lg-3">
                         {/* mb-n1: use negative margin to compensate for font with excessive 
                             margin at the bottom */}
                         <h1 className="text-primary fw-bold my-auto mb-n1">
@@ -39,7 +45,7 @@ export default function ResultsHeader({
                     </div>
                 </div>
                 {/* Action buttons */}
-                <div className="btn-group pb-3" role="group">
+                <div className="btn-group pb-3 mx-3" role="group">
                     {children}
                 </div>
             </div>
