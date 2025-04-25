3f37df1a3f42ba741e3a0065e621b50716d3cc58
Steffen Hirte <shirte@users.noreply.github.com>
2025-04-25 12:19:01 +1200

    fix: Make results table responsive on small screens

--- src/features/resultTable/ResultTable.tsx
+++ src/features/resultTable/ResultTable.tsx
@@ -195,65 +195,67 @@ const ResultTable = memo(function ResultTable({
     }, [module.task, results])
 
     return (
-        <table
-            // align-middle: aligns the text vertically in the middle of the cell
-            //     Note: cells might override this alignment and browsers will complain that
-            //           align-middle has no effect
-            className="table text-center table-sm w-auto align-middle"
-            // TODO: put in class
-            style={{
-                overflowX: "visible",
-                overflowY: "visible",
-            }}
-        >
-            <thead className="sticky-top fs-7">
-                <tr key="firstRow">
-                    {firstColumnRow.map((column: ColumnItem) => (
-                        <th
-                            scope="col"
-                            key={column.name}
-                            rowSpan={column.rowspan}
-                            colSpan={column.colspan}
-                        >
-                            {column.colspan > 1 ? (
-                                <div className="border-bottom border-primary mx-3 py-2">
-                                    {column.visibleName}
-                                </div>
-                            ) : (
-                                column.visibleName
-                            )}
-                        </th>
-                    ))}
-                </tr>
-                {secondColumnRow.length > 0 && (
-                    <tr key="secondRow">
-                        {secondColumnRow.map((column: ColumnItem) => (
+        <div className="table-responsive-xxl">
+            <table
+                // align-middle: aligns the text vertically in the middle of the cell
+                //     Note: cells might override this alignment and browsers will complain that
+                //           align-middle has no effect
+                className="table text-center table-sm w-auto align-middle"
+                // TODO: put in class
+                style={{
+                    overflowX: "visible",
+                    overflowY: "visible",
+                }}
+            >
+                <thead className="sticky-top fs-7">
+                    <tr key="firstRow">
+                        {firstColumnRow.map((column: ColumnItem) => (
                             <th
                                 scope="col"
                                 key={column.name}
                                 rowSpan={column.rowspan}
                                 colSpan={column.colspan}
                             >
-                                {column.visibleName}
+                                {column.colspan > 1 ? (
+                                    <div className="border-bottom border-primary mx-3 py-2">
+                                        {column.visibleName}
+                                    </div>
+                                ) : (
+                                    column.visibleName
+                                )}
                             </th>
                         ))}
                     </tr>
-                )}
-            </thead>
-            <tbody>
-                {resultsGroupedByMolId.map((group: ResultGroup) => (
-                    <TableRowGroup
-                        key={group.mol_id}
-                        group={group}
-                        module={module}
-                        resultProperties={resultProperties}
-                        atomColorProperty={resultProperties.find(
-                            (r) => r.name === atomColorProperty?.name,
-                        )}
-                    />
-                ))}
-            </tbody>
-        </table>
+                    {secondColumnRow.length > 0 && (
+                        <tr key="secondRow">
+                            {secondColumnRow.map((column: ColumnItem) => (
+                                <th
+                                    scope="col"
+                                    key={column.name}
+                                    rowSpan={column.rowspan}
+                                    colSpan={column.colspan}
+                                >
+                                    {column.visibleName}
+                                </th>
+                            ))}
+                        </tr>
+                    )}
+                </thead>
+                <tbody>
+                    {resultsGroupedByMolId.map((group: ResultGroup) => (
+                        <TableRowGroup
+                            key={group.mol_id}
+                            group={group}
+                            module={module}
+                            resultProperties={resultProperties}
+                            atomColorProperty={resultProperties.find(
+                                (r) => r.name === atomColorProperty?.name,
+                            )}
+                        />
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 })
 
