7dadbde2a675af5f595811541f161591ea1597bc
Steffen Hirte <shirte@users.noreply.github.com>
2025-05-25 13:49:24 +1200

    fix: Use prefix slashes in request urls (again) to improve readability

--- src/services/jobs.ts
+++ src/services/jobs.ts
@@ -33,7 +33,7 @@ export const jobsApi = baseApi.injectEndpoints({
                 }
 
                 return {
-                    url: `${moduleId}/jobs`,
+                    url: `/${moduleId}/jobs`,
                     method: "POST",
                     body: form,
                 }
@@ -45,16 +45,16 @@ export const jobsApi = baseApi.injectEndpoints({
         }),
         deleteJob: builder.mutation({
             query: ({ moduleId, jobId }) => ({
-                url: `${moduleId}/jobs/${jobId}`,
+                url: `/${moduleId}/jobs/${jobId}`,
                 method: "DELETE",
             }),
             transformResponse: recursiveSnakeToCamelCase,
         }),
         getJobStatus: websocketQuery({
             builder,
-            query: ({ moduleId, jobId }) => `${moduleId}/jobs/${jobId}`,
+            query: ({ moduleId, jobId }) => `/${moduleId}/jobs/${jobId}`,
             queryWs: ({ moduleId, jobId }) =>
-                `websocket/${moduleId}/jobs/${jobId}`,
+                `/websocket/${moduleId}/jobs/${jobId}`,
             process: (draft, data, complete) => {
                 if (complete) {
                     return
