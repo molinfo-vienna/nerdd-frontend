e26b5a47c10e0749a9ef3aea5d498d4ca79a8a94
Steffen Hirte <shirte@users.noreply.github.com>
2025-06-02 10:46:58 +1200

    fix: Add rehype-slug plugin to mdx

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import mdx from "@mdx-js/rollup"
 import react from "@vitejs/plugin-react"
+import rehypeSlug from "rehype-slug"
 import { visualizer } from "rollup-plugin-visualizer"
 import { defineConfig, type PluginOption } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig(({ mode }) => ({
     plugins: [
-        { enforce: "pre", ...mdx() } as PluginOption,
+        mdx({ rehypePlugins: [rehypeSlug] }) as PluginOption,
         react(),
         visualizer({
             open: true,
