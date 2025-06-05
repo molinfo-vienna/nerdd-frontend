bd8be7b53701d2cf454063f7879b71b6e47c49bb
Steffen Hirte <shirte@users.noreply.github.com>
2025-06-05 13:28:46 +1200

    fix: Give app an ErrorBoundary

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,13 +1,19 @@
 import { ComponentType, PropsWithChildren, ReactNode } from "react"
 import AppRouterProvider from "./AppRouterProvider"
 import DebugProvider from "./DebugProvider"
+import ErrorBoundaryProvider from "./ErrorBoundaryProvider"
 import ReduxProvider from "./ReduxProvider"
 
 type PropsWithRequiredChildren<P = unknown> = P & { children: ReactNode }
 
 type Provider = ComponentType<PropsWithRequiredChildren>
 
-const providers: Provider[] = [ReduxProvider, DebugProvider, AppRouterProvider]
+const providers: Provider[] = [
+    ReduxProvider,
+    DebugProvider,
+    AppRouterProvider,
+    ErrorBoundaryProvider,
+]
 
 export default function Providers({ children }: PropsWithChildren) {
     const Provider = providers.reduceRight(
