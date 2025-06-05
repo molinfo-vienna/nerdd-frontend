3c6f616a4857b8c44068bd052bb648d9698129aa
Steffen Hirte <shirte@users.noreply.github.com>
2025-06-05 16:25:50 +1200

    fix: Move Debug component to DebugProvider

--- src/app/Root.tsx
+++ src/app/Root.tsx
@@ -1,18 +1,5 @@
-import { lazy, Suspense } from "react"
 import { Outlet } from "react-router-dom"
 
-const Debug =
-    import.meta.env.MODE === "development"
-        ? lazy(() => import("@/features/debug/Debug"))
-        : () => <></>
-
 export default function Root() {
-    return (
-        <>
-            <Outlet />
-            <Suspense>
-                <Debug />
-            </Suspense>
-        </>
-    )
+    return <Outlet />
 }
