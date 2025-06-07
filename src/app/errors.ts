b8af7f5efee369af165a522fd77e2259abd8c3e6
Steffen Hirte <shirte@users.noreply.github.com>
2025-06-07 17:01:54 +1200

    fix: Add UnknownError class

--- src/app/errors.ts
+++ src/app/errors.ts
@@ -1,12 +1,28 @@
+const statusMap: Record<number, string> = {
+    0: "Unknown Error",
+    404: "Not Found",
+    500: "Internal Server Error",
+}
+
 export class NerddError extends Error {
     status: number
     statusText: string
     explanation?: string
 
-    constructor(status: number, statusText: string, explanation?: string) {
-        super(statusText)
+    constructor(message: string, status: number, explanation?: string) {
+        super(message)
         this.status = status
-        this.statusText = statusText
+        this.statusText = statusMap[status]
         this.explanation = explanation
+    }
+}
+
+export class UnknownError extends NerddError {
+    constructor() {
+        super(
+            "An unknown error occurred",
+            0,
+            "Please refresh the page or try again later.",
+        )
     }
 }
