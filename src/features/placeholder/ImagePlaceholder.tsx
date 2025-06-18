678dde28ab6003b8115d965daaeb6f07d9e586c9
Steffen Hirte <shirte@users.noreply.github.com>
2025-06-18 17:31:54 +1200

    fix: Implement ImagePlaceholder component

---
+++ src/features/placeholder/ImagePlaceholder.tsx
@@ -0,0 +1,23 @@
+import classNames from "classnames"
+
+type ImagePlaceholderProps = {
+    width?: string
+    height?: string
+}
+
+export default function ImagePlaceholder({
+    width,
+    height,
+}: ImagePlaceholderProps) {
+    return (
+        <div
+            className={classNames("placeholder-glow", {
+                "w-100": width === undefined,
+                "h-100": height === undefined,
+            })}
+            style={{ width: width, height: height }}
+        >
+            <div className="placeholder w-100 h-100"></div>
+        </div>
+    )
+}
