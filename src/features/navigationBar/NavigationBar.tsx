bd921741409c7023421467063529fcef24fd02e8
Steffen Hirte <shirte@users.noreply.github.com>
2025-06-22 21:26:39 +1200

    fix: Add type to breadcrumbElements

--- src/features/navigationBar/NavigationBar.tsx
+++ src/features/navigationBar/NavigationBar.tsx
@@ -16,7 +16,14 @@ export default function NavigationBar() {
     const { jobStatus } = useJobStatus(false)
 
     // configure the breadcrumb elements based on the page
-    let breadcrumbElements
+    let breadcrumbElements: Array<
+        | {
+              name: string
+              url: string
+              type?: "link"
+          }
+        | { type: "divider" }
+    > = []
     let shortNavigation = false
     if (module != undefined) {
         if (pageId == "createJob") {
