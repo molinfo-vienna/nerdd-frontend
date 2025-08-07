a48f3d1a910cf2dd08869eb8f1c37650bc1f698e
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-08 11:35:07 +1200

    fix: Report field-based errors after submission

--- src/pages/CreateJobPage.tsx
+++ src/pages/CreateJobPage.tsx
@@ -15,46 +15,72 @@ export default function CreateJobPage() {
 
     const { module, isLoading } = useModule()
 
-    const onSubmit = useCallback(async (values) => {
-        // input
-        let inputs: string[] = []
-        let sources: string[] = []
-        if (values.inputType === "text") {
-            inputs = [values.input]
-        } else if (values.inputType === "file") {
-            sources = values.inputFile.map((file) => file.sourceData.id)
-        } else if (values.inputType === "draw") {
-            inputs = [values.inputDrawn]
-        } else if (values.inputType === "example") {
-            inputs = [module.exampleSmiles]
-        }
-
-        // all other job parameters
-        const params = Object.fromEntries(
-            module.jobParameters.map((param) => [
-                param.name,
-                values[param.name],
-            ]),
-        )
-
-        const response = await addJob({
-            moduleId: module.id,
-            inputs,
-            sources,
-            params,
-        })
-
-        if (response.error) {
-            console.error(response.error)
-            return {
-                [FORM_ERROR]:
-                    response.error.data?.detail[0].msg ||
-                    "An error occurred while creating the job.",
+    const onSubmit = useCallback(
+        async (values) => {
+            // input
+            let inputs: string[] = []
+            let sources: string[] = []
+            if (values.inputType === "text") {
+                inputs = [values.input]
+            } else if (values.inputType === "file") {
+                sources = values.inputFile.map((file) => file.sourceData.id)
+            } else if (values.inputType === "draw") {
+                inputs = [values.inputDrawn]
+            } else if (values.inputType === "example") {
+                inputs = [module.exampleSmiles]
             }
-        }
 
-        navigate(`/${module.id}/${response.data.id}`)
-    }, [addJob, module, navigate])
+            // all other job parameters
+            const params = Object.fromEntries(
+                module.jobParameters.map((param) => [
+                    param.name,
+                    values[param.name],
+                ]),
+            )
+
+            const response = await addJob({
+                moduleId: module.id,
+                inputs,
+                sources,
+                params,
+            })
+
+            if (response.error) {
+                console.error(response.error)
+
+                let errorMessage = "An error occurred while creating the job."
+                let fieldErrors = {}
+                const detail = response.error.data?.detail
+
+                if (typeof detail === "string") {
+                    // detail is the error message
+                    errorMessage = detail
+                } else if (Array.isArray(detail)) {
+                    // detail is an array of error messages
+                    for (const item of detail) {
+                        if (item.loc?.[0] === "body" && item.loc?.[1]) {
+                            // field-specific error
+                            fieldErrors = {
+                                ...fieldErrors,
+                                [item.loc[1]]: item.msg,
+                            }
+                        } else {
+                            // general error
+                            errorMessage = item.msg
+                        }
+                    }
+                }
+
+                return {
+                    [FORM_ERROR]: errorMessage,
+                    ...fieldErrors,
+                }
+            }
+
+            navigate(`/${module.id}/${response.data.id}`)
+        },
+        [addJob, module, navigate],
+    )
 
     if (isLoading) {
         return LoadingPage()
