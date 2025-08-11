908925a23e5fa41940baa01759a56b3928864588
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-11 22:11:43 +1200

    fix: Show job parameters on results page

--- src/features/resultsHeader/DocsActionButton.tsx
+++ src/features/resultsHeader/DocsActionButton.tsx
@@ -1,21 +1,78 @@
+import { snakeToCamelCase } from "@/services/recursiveSnakeToCamelCase"
+import { Job, JobParameter, Module } from "@/types"
 import { memo } from "react"
-import { FaBookOpen } from "react-icons/fa6"
+import { FaExternalLinkAlt } from "react-icons/fa"
+import { FaRectangleList } from "react-icons/fa6"
 import ActionButton from "./ActionButton"
 
 type DocsActionButtonProps = {
-    moduleId: string
+    module: Module
+    job: Job
 }
 
-function DocsActionButton({ moduleId }: DocsActionButtonProps) {
+function convertParamValueToString(
+    value: any,
+    jobParameter: JobParameter,
+): string {
+    if (value == null) {
+        return "-"
+    }
+    if (jobParameter.type === "boolean" || jobParameter.type === "bool") {
+        return value ? "yes" : "no"
+    } else {
+        return String(value)
+    }
+}
+
+function DocsActionButton({ module, job }: DocsActionButtonProps) {
     return (
-        <ActionButton to={`/${moduleId}/about`} label="Docs">
-            <ActionButton.Icon>
-                <FaBookOpen
-                    viewBox="0 32 576 440"
-                    style={{ width: "1.4em", height: "1em" }}
-                />
-            </ActionButton.Icon>
-        </ActionButton>
+        <div className="btn-group dropdown-center" role="group">
+            <ActionButton
+                label="Details"
+                // dropdown
+                data-bs-toggle="dropdown"
+                data-bs-auto-close="outside"
+                aria-expanded="false"
+            >
+                <ActionButton.Icon>
+                    <FaRectangleList
+                        viewBox="0 32 576 448"
+                        style={{ width: "2em", height: "1em" }}
+                    />
+                </ActionButton.Icon>
+            </ActionButton>
+            <div className="dropdown-menu" style={{ zIndex: 1030 }}>
+                <div>
+                    <h6 className="dropdown-header">Selected parameters</h6>
+                </div>
+                <div className="container px-4">
+                    {module.jobParameters.map((param) => (
+                        <div key={param.name} className="row">
+                            <div className="col-auto">{param.visibleName}:</div>
+                            <div className="col">
+                                {convertParamValueToString(
+                                    job.params[snakeToCamelCase(param.name)],
+                                    param,
+                                )}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="dropdown-divider"></div>
+                <div>
+                    <h6 className="dropdown-header">Documentation</h6>
+                </div>
+                <a
+                    className="dropdown-item text-primary px-4"
+                    href={`/${module.id}/about`}
+                    target="_blank"
+                >
+                    <FaExternalLinkAlt /> More about{" "}
+                    <span className="fw-bold">{module.visibleName}</span> and
+                    its parameters
+                </a>
+            </div>
+        </div>
     )
 }
 
