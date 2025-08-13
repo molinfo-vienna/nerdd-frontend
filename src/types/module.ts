2873b015d65d52239dd4994903c0cd9516562d1e
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-13 18:36:37 +1200

    fix: Add performance characteristics to Module type

--- src/types/module.ts
+++ src/types/module.ts
@@ -24,6 +24,10 @@ export interface Module {
     resultProperties: ResultProperty[]
     partners: Partner[]
     outputFormats: string[]
+    batchSize: number
+    secondsPerMolecule: number
+    startupTimeSeconds: number
+    maxNumMolecules: number
 }
 
 export default Module
