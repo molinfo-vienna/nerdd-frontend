d3ca733e1f559c9db190711cd33ffb280a0b4da6
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-24 11:44:07 +1200

    fix: Improve Publication type

--- src/types/publication.ts
+++ src/types/publication.ts
@@ -1,10 +1,13 @@
+export interface Author {
+    firstName: string
+    lastName: string
+}
+
 export interface Publication {
     title?: string
-    authors?: string
+    authors?: Author[]
     journal?: string
     year?: number
     doi?: string
     url?: string
 }
-
-export default Publication
