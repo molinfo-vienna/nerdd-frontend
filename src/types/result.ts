810eacbfe2afab409e58b69d00ed1533cd86af93
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-25 14:20:09 +1200

    refactor: Make result type more precise

--- src/types/result.ts
+++ src/types/result.ts
@@ -1,5 +1,12 @@
 export interface Result {
-    // Add specific properties when needed
+    id: string
+
+    // these fields are not in camel case, because we leave result records as we get them from the
+    // backend (which uses snake_case)
+    job_id: string
+    mol_id: number
+
+    // other dynamic fields
     [key: string]: any
 }
 
