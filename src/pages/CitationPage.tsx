a40b7fe1c40da8dd0a9dfc550acc1c43ee810181
Steffen Hirte <shirte@users.noreply.github.com>
2025-10-15 22:33:27 +1300

    fix: Add content to citation page

--- src/pages/CitationPage.tsx
+++ src/pages/CitationPage.tsx
@@ -1,5 +1,43 @@
-import ConstructionPage from "./ConstructionPage"
+import ModuleHeader from "@/features/moduleHeader/ModuleHeader"
+import PublicationCard from "@/features/publicationCard/PublicationCard"
+import { useModule } from "@/services"
+import { useState } from "react"
+import Layout from "./Layout"
+import LoadingPage from "./LoadingPage"
 
 export default function CitationPage() {
-    return ConstructionPage()
+    const { module, isLoading } = useModule()
+
+    const [selectedStyle, setSelectedStyle] = useState("apa")
+
+    if (isLoading) {
+        return <LoadingPage />
+    }
+
+    return (
+        <Layout>
+            <Layout.Header>
+                <ModuleHeader
+                    title="Cite"
+                    description={""}
+                    module={module}
+                    subRoute="api"
+                />
+            </Layout.Header>
+            {module != null && (
+                <div className="container py-4">
+                    <div className="row justify-content-center">
+                        <div className="col col-lg-8 col-xl-6">
+                            <PublicationCard
+                                publications={module.publications}
+                                style={selectedStyle}
+                                onSelectStyle={setSelectedStyle}
+                            />
+                        </div>
+                        <div className="col-4 d-none d-lg-block ps-xl-5"></div>
+                    </div>
+                </div>
+            )}
+        </Layout>
+    )
 }
