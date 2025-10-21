ad637a4ec60cc359d0ae7bb50483b4a90dcb5554
Steffen Hirte <shirte@users.noreply.github.com>
2025-10-22 12:15:36 +1300

    fix: Copy nerdd publication to separate file

---
+++ src/features/publicationDialog/nerddPublication.ts
@@ -0,0 +1,110 @@
+export const nerddPublication = {
+    indexed: {
+        "date-parts": [[2025, 9, 30]],
+        "date-time": "2025-09-30T00:10:52Z",
+        timestamp: 1759191052584,
+        version: "3.44.0",
+    },
+    posted: {
+        "date-parts": [[2025, 9, 29]],
+    },
+    "group-title": "Chemistry",
+    publisher: "American Chemical Society (ACS)",
+    license: [
+        {
+            start: {
+                "date-parts": [[2025, 9, 29]],
+                "date-time": "2025-09-29T00:00:00Z",
+                timestamp: 1759104000000,
+            },
+            "content-version": "unspecified",
+            "delay-in-days": 0,
+            URL: "https://creativecommons.org/licenses/by/4.0/",
+        },
+    ],
+    "content-domain": {
+        domain: [],
+        "crossmark-restriction": false,
+    },
+    accepted: {
+        "date-parts": [[2025, 9, 24]],
+    },
+    DOI: "10.26434/chemrxiv-2025-thhkd",
+    type: "posted-content",
+    created: {
+        "date-parts": [[2025, 9, 29]],
+        "date-time": "2025-09-29T13:48:51Z",
+        timestamp: 1759153731000,
+    },
+    source: "Crossref",
+    title: "A Scalable Microservices Platform for Deploying Machine Learning Models in Drug Discovery and Beyond",
+    prefix: "10.26434",
+    author: [
+        {
+            given: "Steffen",
+            family: "Hirte",
+            sequence: "first",
+            affiliation: [
+                {
+                    name: "University of Vienna",
+                },
+            ],
+        },
+        {
+            given: "Vincent-Alexander",
+            family: "Scholz",
+            sequence: "additional",
+            affiliation: [
+                {
+                    name: "University of Vienna",
+                },
+            ],
+        },
+        {
+            ORCID: "https://orcid.org/0000-0003-2667-5877",
+            "authenticated-orcid": false,
+            given: "Johannes",
+            family: "Kirchmair",
+            sequence: "additional",
+            affiliation: [
+                {
+                    name: "University of Vienna",
+                },
+            ],
+        },
+    ],
+    member: "316",
+    "container-title": [],
+    "original-title": [],
+    link: [
+        {
+            URL: "https://chemrxiv.org/engage/api-gateway/chemrxiv/assets/orp/resource/item/68d390643e708a7649f920d3/original/a-scalable-microservices-platform-for-deploying-machine-learning-models-in-drug-discovery-and-beyond.pdf",
+            "content-type": "unspecified",
+            "content-version": "vor",
+            "intended-application": "similarity-checking",
+        },
+    ],
+    deposited: {
+        "date-parts": [[2025, 9, 29]],
+        "date-time": "2025-09-29T13:48:51Z",
+        timestamp: 1759153731000,
+    },
+    score: 1,
+    resource: {
+        primary: {
+            URL: "https://chemrxiv.org/engage/chemrxiv/article-details/68d390643e708a7649f920d3",
+        },
+    },
+    subtitle: [],
+    "short-title": [],
+    issued: {
+        "date-parts": [[2025, 9, 29]],
+    },
+    URL: "http://dx.doi.org/10.26434/chemrxiv-2025-thhkd",
+    relation: {},
+    subject: [],
+    published: {
+        "date-parts": [[2025, 9, 29]],
+    },
+    subtype: "preprint",
+}
