1d929e279d545ad7058e48d6fe54ee8aa035e295
Steffen Hirte <shirte@users.noreply.github.com>
2025-10-22 13:00:07 +1300

    fix: Use citation-js for formatting publications

--- src/features/publicationDialog/PublicationCard.tsx
+++ src/features/publicationDialog/PublicationCard.tsx
@@ -1,79 +1,53 @@
-import { Publication } from "@/types"
-import { Author } from "@/types/publication"
+import { useGetModulePublicationsQuery } from "@/services"
+import { Cite } from "@citation-js/core"
+import "@citation-js/plugin-bibtex"
+import "@citation-js/plugin-csl"
 import hljs from "highlight.js/lib/core"
 import { useMemo } from "react"
 import { FaRegFileAlt } from "react-icons/fa"
 import { FaBook } from "react-icons/fa6"
 import TabCard from "../tabCard/TabCard"
 import bibtex from "./bibtex"
+import { nerddPublication } from "./nerddPublication"
 
 hljs.registerLanguage("bibtex", bibtex)
 
-const BIBTEX_TEMPLATE = `@article{<CITE_KEY>,
-  title={<TITLE>},
-  author={<AUTHOR_LIST>},
-  journal={<JOURNAL>},
-  year={<YEAR>},
-  doi={<DOI>}
-}`
-
-function formatAuthor(author: Author): string {
-    // handle multipe first names like "Jean-Luc"
-    const firstNames = author.firstName.split("-")
-    const initials = firstNames.map((name) => name.charAt(0)).join("-")
-    return `${author.lastName}, ${initials}.`
-}
-
-function transformToApaRaw(publications: Publication[]): string {
+function transformToApaRaw(publications): string {
     return publications
-        .map((pub) => {
-            const authors = pub.authors.map(formatAuthor).join(", ")
-
-            return `${authors} (${pub.year}). ${pub.title}. ${pub.journal}. ${pub.doi}`
-        })
-        .join("\n\n")
-}
-
-function transformToApa(
-    publications: Publication[],
-): JSX.Element[] | JSX.Element {
-    return publications.map((pub, index) => {
-        const authors = pub.authors.map(formatAuthor).join(", ")
-
-        return (
-            <p key={index}>
-                {authors} ({pub.year}). {pub.title}. <em>{pub.journal}</em>.{" "}
-                <a href={`https://doi.org/${pub.doi}`} className="text-nowrap">
-                    {pub.doi}
-                </a>
-            </p>
+        .map((pub) =>
+            new Cite(pub).format("bibliography", {
+                format: "text",
+                template: "apa",
+                lang: "en-US",
+            }),
         )
-    })
-}
-
-function transformToBibtexRaw(publications: Publication[]): string {
-    return publications
-        .map((pub) => {
-            const authorList = pub.authors
-                .map((author) => `${author.lastName}, ${author.firstName}`)
-                .join(" and ")
-
-            const citeKey = `${pub.authors[0].lastName}${pub.year}`
-
-            return BIBTEX_TEMPLATE.replace("<CITE_KEY>", citeKey)
-                .replace("<TITLE>", pub.title)
-                .replace("<AUTHOR_LIST>", authorList)
-                .replace("<JOURNAL>", pub.journal)
-                .replace("<VOLUME>", pub.volume)
-                .replace("<YEAR>", pub.year.toString())
-                .replace("<DOI>", pub.doi)
-        })
         .join("\n\n")
 }
 
-function transformToBibtex(
-    publications: Publication[],
-): JSX.Element[] | JSX.Element {
+function transformToApa(publications): JSX.Element[] | JSX.Element {
+    return (
+        <>
+            {publications.map((pub, i) => (
+                <p
+                    key={i}
+                    dangerouslySetInnerHTML={{
+                        __html: new Cite(pub).format("bibliography", {
+                            format: "html",
+                            template: "apa",
+                            lang: "en-US",
+                        }),
+                    }}
+                />
+            ))}
+        </>
+    )
+}
+
+function transformToBibtexRaw(publications): string {
+    return publications.map((pub) => new Cite(pub).format("bibtex")).join("")
+}
+
+function transformToBibtex(publications): JSX.Element[] | JSX.Element {
     const bibtex = transformToBibtexRaw(publications)
 
     return (
@@ -107,34 +81,33 @@ const mapping = [
     },
 ]
 
-const nerddPublication: Publication = {
-    title: "A Scalable Microservices Platform for Deploying Machine Learning Models in Drug Discovery and Beyond",
-    authors: [
-        { firstName: "Steffen", lastName: "Hirte" },
-        { firstName: "Vincent-Alexander", lastName: "Scholz" },
-        { firstName: "Johannes", lastName: "Kirchmair" },
-    ],
-    journal: "ChemRxiv",
-    year: 2025,
-    doi: "10.26434/chemrxiv-2025-thhkd",
-}
-
 type PublicationCardProps = {
-    publications?: Publication[]
+    moduleId?: string
     style: string
     onSelectStyle: (language: string) => void
 }
 
 export default function PublicationCard({
-    publications,
+    moduleId,
     style,
     onSelectStyle,
 }: PublicationCardProps) {
-    const isLoading = publications == null
+    const {
+        data: publications,
+        isLoading,
+        error,
+    } = useGetModulePublicationsQuery(moduleId)
 
     const styles = useMemo(() => {
         if (isLoading) {
             return []
+        }
+
+        if (error) {
+            return mapping.map(() => ({
+                raw: undefined,
+                content: <p>Error loading publications.</p>,
+            }))
         }
 
         const augmentedPublications = [...publications, nerddPublication]
@@ -143,7 +116,7 @@ export default function PublicationCard({
             raw: transformRaw(augmentedPublications),
             content: transform(augmentedPublications),
         }))
-    }, [publications, isLoading])
+    }, [publications, isLoading, error])
 
     return (
         <TabCard activeTab={style} onSelectTab={onSelectStyle}>
