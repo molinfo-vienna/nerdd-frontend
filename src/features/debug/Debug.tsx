5a3b56fb8f7ea7223fd832ce399407a25b9ea818
Steffen Hirte <shirte@users.noreply.github.com>
2025-11-10 20:26:31 +1300

    fix: Use development hooks in Debug component

--- src/features/debug/Debug.tsx
+++ src/features/debug/Debug.tsx
@@ -1,11 +1,10 @@
-import { useAppDispatch } from "@/app/hooks"
+import { useAppDispatch, useDevelopmentAppSelector } from "@/app/hooks"
 import MockServer from "@/features/mockServer/MockServer"
 import TweakPanel from "@/features/tweakPanel/TweakPanel"
 import { useDebounce, useLocalStorage } from "@uidotdev/usehooks"
 import { useEffect, useState } from "react"
 import { makeButton, useTweaks } from "use-tweaks"
 import { setNumModules } from "./debugSlice"
-import { useDebugSelector } from "./useDebugSelector"
 
 type DebugProps = {
     children?: React.ReactNode
@@ -23,8 +22,10 @@ type DebugSettings = {
 
 export default function Debug({ children }: DebugProps) {
     const dispatch = useAppDispatch()
-    const moduleConfigs = useDebugSelector((state) => state.debug.moduleConfigs)
-    const jobs = useDebugSelector((state) => state.debug.jobs)
+    const moduleConfigs = useDevelopmentAppSelector(
+        (state) => state.debug.moduleConfigs,
+    )
+    const jobs = useDevelopmentAppSelector((state) => state.debug.jobs)
 
     const [settings, setSettings] = useLocalStorage<DebugSettings>("debug", {
         // whether the mock server should be used (instead of the real API)
