32a84ff5c93f103dadcfe49a4917534e79a0a07f
Steffen Hirte <shirte@users.noreply.github.com>
2025-11-12 20:51:37 +1300

    fix: Add types to sources api service

--- src/services/sources.ts
+++ src/services/sources.ts
@@ -1,9 +1,19 @@
-import { baseApi } from "./base"
+import { Source } from "@/types"
+import { baseApi } from "./api"
 import recursiveSnakeToCamelCase from "./recursiveSnakeToCamelCase"
+
+export interface AddSourceArgs {
+    file: File
+    onUploadProgress: (uploadProgress: number) => void
+}
+
+export interface DeleteSourceArgs {
+    sourceId: string
+}
 
 export const sourcesApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
-        addSource: builder.mutation({
+        addSource: builder.mutation<Source, AddSourceArgs>({
             query: ({ file, onUploadProgress }) => {
                 const body = new FormData()
                 body.append("file", file)
@@ -17,7 +27,7 @@ export const sourcesApi = baseApi.injectEndpoints({
             },
             transformResponse: recursiveSnakeToCamelCase,
         }),
-        deleteSource: builder.mutation({
+        deleteSource: builder.mutation<Source, DeleteSourceArgs>({
             query: ({ sourceId }) => ({
                 url: `/sources/${sourceId}`,
                 method: "DELETE",
