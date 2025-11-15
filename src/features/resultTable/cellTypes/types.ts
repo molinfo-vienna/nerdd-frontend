66de2d47c4d43454584f50cb6e38a15428b7f967
Steffen Hirte <shirte@users.noreply.github.com>
2025-11-16 11:34:09 +1300

    refactor: Define common type for table cell props

---
+++ src/features/resultTable/cellTypes/types.ts
@@ -0,0 +1,18 @@
+import type { Module, Result } from "@/types"
+import type React from "react"
+import type { AugmentedResultProperty, ResultGroup } from "../resultTableSlice"
+
+export type CellRendererProps = {
+    module: Module
+    result: Result
+    resultProperty: AugmentedResultProperty
+    group: ResultGroup
+    selectedAtom?: number
+    className?: string
+    onAtomSelect?: (atomId?: number) => void
+    propertyPalettes: Record<string, any>
+    value: any
+    compressed: boolean
+}
+
+export type CellRenderer = (props: CellRendererProps) => React.ReactNode
